850749870d8b25b2e6c676d3140302e720149145
Fanelesiyibonge Mabuza <menzifm12@gmail.com>
2025-10-03 06:12:28 +0200

    Vercel Error Solve Database definition 2

--- carfinity/settings.py
+++ carfinity/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'carfinity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 }
