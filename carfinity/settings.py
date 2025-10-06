47959cac7ebd915b475d572fc32a7dd046137e5e
Fanelesiyibonge Mabuza <menzifm12@gmail.com>
2025-10-06 06:06:13 +0200

    Dashboards Backend fix error

--- carfinity/settings.py
+++ carfinity/settings.py
@@ -13,7 +13,7 @@ import dj_database_url
 from pathlib import Path
 import os
 #from decouple import config
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
 
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+#load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
